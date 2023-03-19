eaffa1db3127b17389dabd1cc7a7a14b55a01466
jhoon9494 <wjdgns9406@gmail.com>
2023-03-19 21:18:19 +0900

    feat: 주문 목록 페이지 및 페이지네이션 버튼 컴포넌트 구현

---
+++ src/components/PageButtonList.tsx
@@ -0,0 +1,52 @@
+import { useSearchParams } from 'react-router-dom';
+import { Button, HStack } from '@chakra-ui/react';
+import useFetch from '@/lib/hooks/usePagination';
+import { generatePageList } from '@/lib/utils/pagenationHelper';
+
+const PageButtonList = () => {
+  const { startPage, endPage, pageSize } = useFetch();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const pageNumber = searchParams.get('page');
+  const pageList = generatePageList(startPage, endPage);
+
+  const onMovePage = (number: number, option?: number) => {
+    if (option) {
+      number += option;
+    }
+    const page = number.toString();
+    setSearchParams({ page });
+  };
+
+  return (
+    <HStack>
+      <Button
+        type="button"
+        onClick={() => onMovePage(Number(pageNumber), -1)}
+        isDisabled={Number(pageNumber) === 1 || !pageNumber}
+      >
+        prev
+      </Button>
+      {pageList.map((page) => {
+        return (
+          <Button
+            key={`${page}-button`}
+            type="button"
+            isActive={page === Number(pageNumber)}
+            onClick={() => onMovePage(page)}
+          >
+            {page}
+          </Button>
+        );
+      })}
+      <Button
+        type="button"
+        onClick={() => onMovePage(Number(pageNumber), +1)}
+        isDisabled={Number(pageNumber) === pageSize}
+      >
+        next
+      </Button>
+    </HStack>
+  );
+};
+
+export default PageButtonList;
---
+++ src/pages/Admin.tsx
@@ -0,0 +1,53 @@
+import {
+  TableContainer,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Heading,
+  VStack,
+} from '@chakra-ui/react';
+import PageButtonList from '@/components/PageButtonList';
+import usePagination from '@/lib/hooks/usePagination';
+
+const Admin = () => {
+  const { pageData } = usePagination();
+  return (
+    <VStack maxW="1280px" mx="auto" py="10">
+      <Heading>주문 목록</Heading>
+      <TableContainer w="100%">
+        <Table variant="striped">
+          <Thead>
+            <Tr>
+              <Th>주문 번호</Th>
+              <Th>거래 시간</Th>
+              <Th>주문 처리상태</Th>
+              <Th>고객 번호</Th>
+              <Th>고객 이름</Th>
+              <Th>가격</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {pageData.map((data) => {
+              return (
+                <Tr key={`order-${data.id}`}>
+                  <Td>{data.id}</Td>
+                  <Td>{data.transaction_time}</Td>
+                  <Td>{data.status ? 'True' : 'False'}</Td>
+                  <Td>{data.customer_id}</Td>
+                  <Td>{data.customer_name}</Td>
+                  <Td>{data.currency}</Td>
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
+      </TableContainer>
+      <PageButtonList />
+    </VStack>
+  );
+};
+
+export default Admin;
