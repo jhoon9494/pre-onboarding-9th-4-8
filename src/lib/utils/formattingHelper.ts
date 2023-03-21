97d3f77fb4cf09ef83ee2186bbad60b65a00f7b7
jhoon9494 <wjdgns9406@gmail.com>
2023-03-21 16:02:38 +0900

    refactor: formattingHelper type 수정
    fetch data type 설정에 따른 type 수정

--- src/lib/utils/formattingHelper.ts
+++ src/lib/utils/formattingHelper.ts
@@ -2,7 +2,7 @@ import { ITEMS_PER_PAGE } from '@/constants/units';
 
 export const formatDollarToNumber = (str: string) => Number(str.split('$')[1]);
 
-export const formatNumToDollar = (num: number) =>
+export const formatNumToDollar = (num: number | undefined = 0) =>
   `$ ${num.toLocaleString('en')}`;
 
 export const formatDate = (date: Date) =>
@@ -10,8 +10,8 @@ export const formatDate = (date: Date) =>
 
 export const formatPageInfo = (
   currentPage: number,
-  currentLength: number,
-  totalLength: number,
+  currentLength: number | undefined = 0,
+  totalLength: number | undefined = 0,
 ) =>
   `Showing ${currentPage * ITEMS_PER_PAGE - (ITEMS_PER_PAGE - 1)} - ${
     currentPage * ITEMS_PER_PAGE - ITEMS_PER_PAGE + currentLength
